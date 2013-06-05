26e0cc725d12b9bbc9b114cc5610d37dc4b9ed05
abcdw <andrewtropin@gmail.com>
2013-06-06 00:52:47 +0400

    Create loginWidget and implement init function for loginWidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,49 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    confFileName = "server.conf";
+
+    initLoginWidget();
+
+    loadSettings();
+    setCentralWidget(loginWidget);
+}
+
+void MainWindow::initLoginWidget()
+{
+    loginWidget       = new QWidget(this);
+    loginWidgetLayout = new QGridLayout(loginWidget);
+    loginLine         = new QLineEdit(loginWidget);
+    passwordLine      = new QLineEdit(loginWidget);
+    loginButton       = new QPushButton("login", loginWidget);
+
+    QLabel *loginLabel    = new QLabel("login:", loginWidget);
+    QLabel *passwordLabel = new QLabel("pass:", loginWidget);
+
+    passwordLine->setEchoMode(QLineEdit::Password);
+
+    loginWidgetLayout->addWidget(loginLabel,    0, 0);
+    loginWidgetLayout->addWidget(passwordLabel, 1, 0);
+    loginWidgetLayout->addWidget(loginLine,     0, 1);
+    loginWidgetLayout->addWidget(passwordLine,  1, 1);
+    loginWidgetLayout->addWidget(loginButton,   2, 0, 1, 2);
+}
+
+void MainWindow::loadSettings()
+{
+    QSettings settings(confFileName, QSettings::IniFormat);
+
+
+}
+
+void MainWindow::saveSettings()
+{
+    QSettings settings(confFileName, QSettings::IniFormat);
+
+    settings.setValue("auth/login", "root");
+    settings.setValue("auth/pass" , "root");
+    settings.setValue("auth/db"   , "auth");
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,30 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QSettings>
+#include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+#include <QGridLayout>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+private:
+    void loadSettings();
+    void saveSettings();
+    void initLoginWidget();
+
+    QString confFileName;
+
+    QWidget     *loginWidget;
+    QLineEdit   *loginLine;
+    QLineEdit   *passwordLine;
+    QGridLayout *loginWidgetLayout;
+    QPushButton *loginButton;
+
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
